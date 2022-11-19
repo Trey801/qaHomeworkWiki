cd6e851394e261c1a22063bb665718803fef505a
Trey801 <treylandgrebe1@yahoo.com>
2022-11-18 18:45:56 -0700

    setupPractice ts

---
+++ setupPractice/emFancyTest.test.ts
@@ -0,0 +1,48 @@
+import {Builder, By, Capabilities, until, WebDriver} from "selenium-webdriver"
+const chromedriver = require("chromedriver")
+const driver: WebDriver = new Builder(). withCapabilities(Capabilities.chrome()).build()
+import {emManager} from './employeeManagerPageObject'
+const employeePage = new emManager(driver)
+
+class Inturns {
+    name: string;
+    phone: number;
+    title: string;
+    
+
+    constructor(name:string, phone: number, title: string){
+        this.name = name;
+        this.phone = phone;
+        this.title = title;
+    }
+}
+
+let newInturns: Array<Inturns> = [
+    new Inturns("Blossom", 1234567890, "Power Puff Girl"),
+    new Inturns("Buttercup", 1234567890, "Power Puff Girl"),
+    new Inturns("Bubbles", 1234567890, "Power Puff Girl"),
+    new Inturns("Miss Sara Bellum", 1234567890, "Secretary")
+]
+
+let addEmp = async (newInturns) => {
+    await employeePage.click(employeePage.addEmployee)
+    await employeePage.click(employeePage.newEmployee)
+    await employeePage.click(employeePage.nameField)
+    await employeePage.setInput(employeePage.nameField, Inturns.name)
+    await employeePage.click(employeePage.phoneField)
+    await employeePage.setInput(employeePage.phoneField, Inturns.phone)
+    await employeePage.click(employeePage.titleField)
+    await employeePage.setInput(employeePage.titleField, newInturns.title)
+    await employeePage.click(employeePage.saveBtn)
+    await employeePage.driver.sleep(3000)
+}
+
+describe("should add employees to employee manager", () => {
+    test("can add emplyees using array", async () => {
+        await employeePage.navigate()
+        for(let i = 0; i < newInturns.length; i++){
+            await addEmp(newInturns[i])
+        }
+        await employeePage.driver.quit()
+    })
+})
